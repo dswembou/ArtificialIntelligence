6b3f5ef2a334fb20c7d4c85d22ca8843d03edeaa
DSWE <dswe@mboutrecht.nl>
2020-11-20 13:26:17 +0100

    Project voor opdracht HatTrick AI

---
+++ Assets/Hat Trick - The Catch Game/Scripts/HT_AI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HT_AI : MonoBehaviour
+{
+
+    private HT_GameController GC;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        
+    }
+}
---
+++ Assets/Hat Trick - The Catch Game/Scripts/HT_DestroyOnContact.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HT_DestroyOnContact : MonoBehaviour
+{
+	private HT_GameController GM;
+
+	private void Start()
+	{
+		GM = GameObject.FindObjectOfType<HT_GameController>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Destroy (other.gameObject);
+	}
+}
